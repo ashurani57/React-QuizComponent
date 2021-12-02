9bf955aed170a941e2ab0e0aed7c1b40bc382583
ashurani57 <78693891+ashurani57@users.noreply.github.com>
2021-12-02 12:38:08 +0530

    Quiz1

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,14 +3,14 @@ import QuizQuestionButton from "./QuizQuestionButton.js";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
-    this.state = { incorrectAnswer: true };
+    this.state = { incorrectAnswer: false };
   }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.setState = { incorrectAnswer: false };
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
     } else {
-      this.setState = { incorrectAnswer: true };
+      this.setState({ incorrectAnswer: true });
     }
   }
   render() {
@@ -41,4 +41,5 @@ class QuizQuestion extends Component {
     );
   }
 }
+
 export default QuizQuestion;
